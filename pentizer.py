7ce477aa69d4600e532d06b1af665673bc634e11
mixalis <m.tsimpliarakis@gmail.com>
2021-11-01 22:12:17 +0200

    backbox update

--- pentizer.py
+++ pentizer.py
@@ -10,11 +10,12 @@ def welcome():
 
 def goodbye():
     print("\nDon't forget to perform 'sudo apt update'")
-    print('Thank you for using @@@. Happy hacking!\n')
+    print('Thank you for using Pentizer. Happy hacking!\n')
 
 def menu():
     print('==================================')
     print('|                                |')
+    print('| 0. Clean repositories and Exit |')
     print('| 1. Add Kali Linux repositories |')
     print('| 2. Add Parrot Os repositories  |')
     print('|                                |')
@@ -28,26 +29,43 @@ def detect():
     if parrot in open('/etc/apt/sources.list').read():
         print ('Parrot repositories are already on your system')
 
-def repo_clean():
-    os.system("sed -i '/deb http://http.kali.org/kali kali-rolling main non-free contrib/d' /etc/apt/sources.list")
-    os.system("sed -i '/deb https://deb.parrotsec.org/parrot parrot main contrib non-free/d' /etc/apt/sources.list")
+def clean():
+    os.system("sudo sed -i '/kali kali-rolling main non-free contrib/d' /etc/apt/sources.list")
+    os.system("sudo sed -i '/parrot parrot main contrib non-free/d' /etc/apt/sources.list")
+    os.system("sudo sed -i '/parrot parrot-security main contrib non-free/d' /etc/apt/sources.list")
+    os.system("sudo sed -i '/backbox/d' /etc/apt/sources.list")
+    os.system("sudo rm -f /etc/apt/trusted.gpg.d/kali-archive-key.asc")
+    os.system("sudo rm -f /etc/apt/trusted.gpg.d/parrotsec.gpg")
 
 def main():
     choice = int(input('--> '))
-    if choice == 1:
+    if choice == 0:
+    	goodbye()
+    	clean()
+    	exit()
+    elif choice == 1:
         with open('/etc/apt/sources.list', 'a') as f:
             f.write('deb http://http.kali.org/kali kali-rolling main non-free contrib\n')
+            f.write('deb-src http://http.kali.org/kali kali-rolling main non-free contrib\n')
             fullfilename = os.path.join('/etc/apt/trusted.gpg.d', 'kali-archive-key.asc')
             urllib.request.urlretrieve('https://archive.kali.org/archive-key.asc', filename=fullfilename)
             #os.system('sudo apt-key add /etc/apt/trusted.gpg.d/kali-archive-key.asc')
     elif choice == 2:
         with open('/etc/apt/sources.list', 'a') as f:
             f.write('deb https://deb.parrotsec.org/parrot parrot main contrib non-free\n')
-            fullfilename = os.path.join('/etc/apt/trusted.gpg.d', 'parrotsec.gpg')
-            urllib.request.urlretrieve('https://archive.parrotsec.org/parrot/misc/parrotsec.gpg', filename=fullfilename)
+            f.write('deb https://deb.parrotsec.org/parrot parrot-security main contrib non-free\n')
+            f.write('deb-src https://deb.parrotsec.org/parrot parrot main contrib non-free\n')
+            f.write('deb-src https://deb.parrotsec.org/parrot parrot-security main contrib non-free\n')
+            #fullfilename = os.path.join('/etc/apt/trusted.gpg.d', 'parrotsec.gpg')
+            #urllib.request.urlretrieve('https://archive.parrotsec.org/parrot/misc/parrotsec.gpg', filename=fullfilename)
             #os.system('sudo apt-key add /etc/apt/trusted.gpg.d/parrotsec.gpg')
+    elif choice == 3:
+        with open('/etc/apt/sources.list', 'a') as f:
+            f.write('deb http://ppa.launchpad.net/backbox/seven/ubuntu focal main\n')
+            f.write('deb-src http://ppa.launchpad.net/backbox/seven/ubuntu focal main\n')
     else:
         print('\nInvalid option\nExiting...\n')
+        clean()
         exit()
 
 if __name__ == "__main__":
@@ -58,4 +76,3 @@ if __name__ == "__main__":
     menu()
     detect()
     main()
-    goodbye()
